2b5bceaa0291e22e1d6bcd908d800996480c4fd6
Rafael Ferreira <rafael.low1@gmail.com>
2025-10-27 20:49:24 -0300

    create swa

--- infra/backend.tf
+++ infra/backend.tf
@@ -6,7 +6,7 @@ terraform {
     container_name       = "statetf"
     key                  = "infra.terraform.tfstate"
     
-    # Usar Azure AD auth (OIDC do GitHub via azure/login)
+    # Usar Azure AD auth (OIDC do GitHub via azure)
     use_azuread_auth     = true
   }
 }
