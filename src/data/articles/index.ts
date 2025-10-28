d1906fdee2eb139983d5a8dfad0839d735fcae3b
Rafael Ferreira <rafael.low1@gmail.com>
2025-10-28 10:02:21 -0300

    update

--- src/data/articles/index.ts
+++ src/data/articles/index.ts
@@ -3,14 +3,27 @@ import { Article } from './types';
 // Import all article files
 import { article as azureBackupVMs } from './2024-01-15-azure-backup-virtual-machines';
 import { article as azurePolicyVisaoCompleta } from './2025-01-11-azure-policy-visao-completa';
+import { article as automatizandoInfraestruturaAgeis } from './2024-10-29-automatizando-infraestrutura-metodologias-ageis';
+import { article as explorandoIAGenerativa } from './2023-10-27-explorando-ia-generativa';
+import { article as estrategiasModernizacao6Rs } from './2024-08-22-estrategias-modernizacao-6rs';
+import { article as azureWorkbooksFinOps } from './2024-08-04-azure-workbooks-finops';
+import { article as guiaCriacaoVMsAzure } from './2024-07-26-guia-criacao-vms-azure';
+import { article as antesCloudNativeFundacao } from './2024-07-14-antes-cloud-native-fundacao-solida';
+import { article as monitoriaObservabilidade } from './2024-07-14-monitoria-observabilidade-cloud';
+import { article as fundacaoSolidaFinOps } from './2024-07-14-fundacao-solida-finops';
 
 // Consolidated array of all articles
 export const allArticles: Article[] = [
   azureBackupVMs,
   azurePolicyVisaoCompleta,
-  // TODO: Add remaining articles here as they are created
-  // The format should be: import { article as variableName } from './YYYY-MM-DD-slug';
-  // Then add variableName to the array above
+  automatizandoInfraestruturaAgeis,
+  explorandoIAGenerativa,
+  estrategiasModernizacao6Rs,
+  azureWorkbooksFinOps,
+  guiaCriacaoVMsAzure,
+  antesCloudNativeFundacao,
+  monitoriaObservabilidade,
+  fundacaoSolidaFinOps,
 ].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
 // Helper function to get article by slug
