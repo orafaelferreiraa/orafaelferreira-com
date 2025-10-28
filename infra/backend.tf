2e49b8c3f90463dc4bf2931be09a0182924cbba2
Rafael Ferreira <rafael.low1@gmail.com>
2025-10-27 21:33:11 -0300

    create swa

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,13 +1,10 @@
 terraform {
   backend "azurerm" {
-    # Storage Account onde o state será armazenado
     resource_group_name  = "rg-site"
     storage_account_name = "stostateorafael"
     container_name       = "statetf"
     key                  = "infra.terraform.tfstate"
     
-    # Usar Azure AD (RBAC) no plano de dados do Storage com Service Principal
-    # Requer papel "Storage Blob Data Contributor" para o SP neste Storage Account
     use_azuread_auth     = true
   }
 }
