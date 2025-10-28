30adfe5d19a670b2ea0328c013b833d4c31518a3
Rafael Ferreira <rafael.low1@gmail.com>
2025-10-27 21:55:35 -0300

    create swa

--- infra/backend.tf
+++ infra/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "stostateorafael"
     container_name       = "statetf"
     key                  = "infra.terraform.tfstate"
+    use_azuread_auth     = true
   }
 }
