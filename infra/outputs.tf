5018af501d27b9f731fd7d019a99bfc9b63fd519
Rafael Ferreira <rafael.low1@gmail.com>
2025-11-15 12:42:36 -0300

    removing tags

--- infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "static_web_app_default_hostname" {
-  value       = azurerm_static_web_app.this.default_host_name
-  description = "Default hostname of the Azure Static Web App."
-}
-
-output "static_web_app_id" {
-  value       = azurerm_static_web_app.this.id
-  description = "Resource ID of the Azure Static Web App."
-}
