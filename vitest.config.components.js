6c230d2dbcedb033b43f17de83a80e9f1851b752
Rafael Ferreira <rafael.low1@gmail.com>
2025-12-30 13:59:16 -0300

    upgrade

--- vitest.config.components.js
+++
@@ -1,8 +0,0 @@
-export default {
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-    include: ['src/components/**/*.test.{ts,tsx}'],
-    reporters: process.env.CI ? ['dot'] : ['default'],
-  },
-};
