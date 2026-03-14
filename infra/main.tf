a1f1f2ef89ec46ebeacd7c5a60235c1170e80ab6
Rafael Ferreira <rafael.low1@gmail.com>
2026-03-14 17:05:01 -0300

    refactor: remove unused repository variables from Terraform configurations

--- infra/main.tf
+++ infra/main.tf
@@ -10,11 +10,6 @@ resource "azurerm_static_web_app" "this" {
 
   sku_tier = "Free"
   sku_size = "Free"
-
-  repository_url    = var.repository_url != "" ? var.repository_url : null
-  repository_branch = var.repository_branch != "" ? var.repository_branch : null
-  repository_token  = var.repository_token != "" ? var.repository_token : null
-
 }
 
 resource "azurerm_static_web_app_custom_domain" "txt-value" {
