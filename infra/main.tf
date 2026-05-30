86e9509f65ddb37e3a40e217d8166a036f170965
Rafael Ferreira <rafael.low1@gmail.com>
2026-05-30 18:56:48 -0300

    refactor(infra): remove unused apex custom domain resource

--- infra/main.tf
+++ infra/main.tf
@@ -73,16 +73,3 @@ resource "azurerm_static_web_app_custom_domain" "www" {
     azurerm_dns_cname_record.www,
   ]
 }
-
-resource "azurerm_static_web_app_custom_domain" "apex" {
-  count             = var.apex_domain_validation_token != "" ? 1 : 0
-  provider          = azurerm.site
-  static_web_app_id = azurerm_static_web_app.this.id
-  domain_name       = "orafaelferreira.com"
-  validation_type   = "dns-txt-token"
-
-  depends_on = [
-    azurerm_dns_a_record.apex,
-    azurerm_dns_txt_record.apex_validation[0],
-  ]
-}
