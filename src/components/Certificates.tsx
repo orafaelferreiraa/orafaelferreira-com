7ea6c486c8a27c89688e04c616a14240504ca03b
Rafael Ferreira <117859338+orafaelferreiraa@users.noreply.github.com>
2026-06-16 19:37:42 -0300

    AI Capabilities and Limitations

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -15,6 +15,9 @@ interface YearCertificates {
 const certificatesByYear: YearCertificates[] = [{
   year: "2026",
   certificates: [{
+       title: "AI Capabilities and Limitations",
+    link: "https://verify.skilljar.com/c/cjygngqtj9v6"
+  },{
        title: "Introduction to subagents",
     link: "https://verify.skilljar.com/c/ywrj3uu7es85"
   },{
