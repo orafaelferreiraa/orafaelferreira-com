40a361bbcd2f499dfb9bb065f51510c19d1eba78
Rafael Ferreira <117859338+orafaelferreiraa@users.noreply.github.com>
2026-06-24 12:14:44 -0300

    AI Fluency for educators

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -15,6 +15,9 @@ interface YearCertificates {
 const certificatesByYear: YearCertificates[] = [{
   year: "2026",
   certificates: [{
+       title: "AI Fluency for educators",
+    link: "https://verify.skilljar.com/c/xxw3gnxsmy6i"
+  }{
        title: "AI Fluency for students",
     link: "https://verify.skilljar.com/c/x229iwszrm2x"
   },{
