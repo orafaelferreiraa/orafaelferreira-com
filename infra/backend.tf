63ad35b8736c3178bf0da0411499e867d1436a2a
Rafael Ferreira <rafael.low1@gmail.com>
2026-08-31 14:11:01 -0300

    stostateorafael2

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-site"
-    storage_account_name = "stostateorafael"
+    storage_account_name = "stostateorafael2"
     container_name       = "statetf"
     key                  = "infra.terraform.tfstate"
     use_azuread_auth     = true
